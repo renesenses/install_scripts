ad685937a6d472498f86f4764224892298e16bf2
Bertrand Clech <bertrand@VM-DEV.local>
2015-04-13 23:00:08 +0200

    dying for cpan

--- install_for_cpan.sh
+++ install_for_cpan.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
-# Requirements for cpan
+# Requirements for perl dev and cpan
 
+cd ~/GIT_REPO/install_scripts
+
+brew install wxWidget
 brew install wget
 brew install gnutls
 brew install gnupg
 
 brew install homebrew/dupes/diffutils
+
+sudo cpan -i Task::CPAN::Reporter
+sudo cpan -i YAML
+sudo cpan -i CPAN::SQLite
+
+
+install_makepatch.sh
+
+sudo cpan -i Alien::wxWidgets
+sudo cpan -i Wx
+
+cpan -o conf init 
